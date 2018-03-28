56e31c19dcffa6da884def3e66f70215336f761f
your name <460319984@qq.com>
2018-03-29 00:34:05 +0800

    upload

--- toutiao/src/main/java/com/nowcoder/service/NewsService.java
+++ toutiao/src/main/java/com/nowcoder/service/NewsService.java
@@ -2,10 +2,17 @@ package com.nowcoder.service;
 
 import com.nowcoder.dao.NewsDAO;
 import com.nowcoder.model.News;
+import com.nowcoder.util.ToutiaoUtil;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.web.multipart.MultipartFile;
 
+import java.io.File;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.StandardCopyOption;
 import java.util.List;
+import java.util.UUID;
 
 /**
  * Created by licker .
@@ -17,5 +24,20 @@ public class NewsService {
 
     public List<News> getLatestNews(int userId, int offset, int limit) {
         return newsDAO.selectByUserIdAndOffset(userId, offset, limit);
+    }
+    public String saveImage(MultipartFile file) throws IOException {
+        int dotPos = file.getOriginalFilename().lastIndexOf(".");
+        if (dotPos < 0) {
+            return null;
+        }
+        String fileExt = file.getOriginalFilename().substring(dotPos + 1).toLowerCase();
+        if (!ToutiaoUtil.isFileAllowed(fileExt)) {
+            return null;
+        }
+
+        String fileName = UUID.randomUUID().toString().replaceAll("-", "") + "." + fileExt;
+        Files.copy(file.getInputStream(), new File(ToutiaoUtil.IMAGE_DIR + fileName).toPath(),
+                StandardCopyOption.REPLACE_EXISTING);
+        return ToutiaoUtil.TOUTIAO_DOMAIN + "image?name=" + fileName;
     }
 }
